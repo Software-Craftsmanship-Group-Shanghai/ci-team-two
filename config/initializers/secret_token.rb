9d6fec5273c4ba1ce9473d7383356be97d23cceb
loveky <ylzcylx@gmail.com>
2014-02-16 11:03:09 +0800

    initial drop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CITest::Application.config.secret_key_base = '9d22e37cbe3aef63f6d07d8103682fac77e6a0e1da99fae527960db8ce4ef486e76eb227f414abc3bd68ef28fc4123bbdb73599f8185b810e77b7987235ca5f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CITest::Application.config.session_store :cookie_store, key: '_CI-test_session'

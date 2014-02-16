414aebf1c5e91300df925def2d454bdd32f65be0
loveky <ylzcylx@gmail.com>
2014-02-16 14:53:47 +0800

    add regist token function  by Jarvis

--- app/controllers/regist_controller.rb
+++ app/controllers/regist_controller.rb
@@ -3,7 +3,8 @@ class RegistController < ApplicationController
   end
 
   def do_regist
-    user = User.new(email: params[:email], password: params[:password])
+    token = Base64.encode64(params[:email] + params[:password] + Time.now.to_s) 
+    user = User.new(email: params[:email], password: params[:password], register_token: token)
     user.save!
   end
 end
